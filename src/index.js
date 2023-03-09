d003e2d4d790047b1104baa75df884b3331b8bfe
jesusrobot <jesusrobot9000@gmail.com>
2023-03-08 21:03:46 -0600

    removes some useless commented code snippets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 const Server = require('./server')
 
 const server = new Server()
-
-// const express = require('express')
-// const app = express()
-
-// const routes = require('./routes/routes')
-// const port = process.env.PORT || 3000
-
-// app.use(express.json())
-
-// app.use('/whatsapp', routes)
-
-// app.listen(port, () => {console.log(`🌎 Server running on port ${port}`)})
